339b06a5d8744c0013f37b7abfc0b5ef708b0c03
ymm2110 <yuntongman17@gmail.com>
2019-01-16 14:50:07 -0500

    Revert " completed CLI"
    This reverts commit 71983123978342c8f71bbde2d6830c8a77cb3177.

--- lib/new-opportunities/opportunity.rb
+++ lib/new-opportunities/opportunity.rb
@@ -16,9 +16,9 @@ class NewOpportunities::Opportunities
       opportunity = NewOpportunities::Opportunities.new
       opportunity.name = post.css("h4 a").text.strip
       opportunity.url = post.css("h4 a").attr("href").value
-      opportunity.description = post.css("p.eltdf-post-excerpt").text.strip.gsub("\r\n", " ").split(" ")[4..-1].join(" ")
+      opportunity.description = post.css("p.eltdf-post-excerpt").text.strip.gsub("\r\n", " ")
       opportunity.post_date = post.css(".entry-date a").text.strip
-      opportunity.deadline = post.css("p.eltdf-post-excerpt").text.strip.gsub("\r\n", " ").split(" ")[0..3].join(" ")
+      opportunity.deadline = opportunity.description.split(" ")[0..3].join(" ")
       self.all << opportunity
     end
   end
